87b1bcd7d84aab68bf1716632053bd803b7f064c
bmustill-rose <services+github@benmr.com>
2022-03-23 12:39:54 +0000

    First stab at digitsToBeeps

--- utils.py
+++ utils.py
@@ -1,3 +1,14 @@
+import time
+
+def digitsToBeeps(clue, number, mappings):
+ for pos, num in enumerate(str(number)):
+  repeatTone(clue, mappings[pos], int(num))
+
+def repeatTone (clue, tone, times):
+ for i in range(0, times):
+  clue.play_tone(tone[0], tone[1])
+  time.sleep(tone[2])
+
 def playStartupSound(clue, sequence):
  for note in sequence:
   clue.play_tone(note[0], note[1])
