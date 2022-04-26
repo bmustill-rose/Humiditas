6b324c209fd206c127eb16622c9f7bb6cd651214
bmustill-rose <services+github@benmr.com>
2022-04-26 17:42:24 +0100

    Replace Ready with Ok. Don't print colour values to serial in lightprobe

--- code.py
+++ code.py
@@ -10,12 +10,12 @@ import utils
 time.sleep(0.5)
 utils.playStartupSound(clue, config.startupSound)
 display = clue.simple_text_display(colors=[config.textColour], text_scale=config.textSize)
-display[0].text = "Ready"
-print(json.dumps("ready"))
+display[0].text = "Ok"
+print(json.dumps("Ok"))
 
 while True:
  display.show()
- if clue.button_a and clue.button_b: light.lightProbe(clue, display); display[0].text="Ready"
- elif clue.button_a: utils.surfaceSensorData(clue, display, {"name": "humidity", "value": round(clue.humidity)}, config.noteDigitMappings); display[0].text="Ready"
- elif clue.button_b: utils.surfaceSensorData(clue, display, {"name": "temperature", "value": round(clue.temperature)}, config.noteDigitMappings); display[0].text="Ready"
+ if clue.button_a and clue.button_b: light.lightProbe(clue, display); display[0].text="Ok"
+ elif clue.button_a: utils.surfaceSensorData(clue, display, {"name": "humidity", "value": round(clue.humidity)}, config.noteDigitMappings); display[0].text="Ok"
+ elif clue.button_b: utils.surfaceSensorData(clue, display, {"name": "temperature", "value": round(clue.temperature)}, config.noteDigitMappings); display[0].text="Ok"
  time.sleep(0.1)
--- light.py
+++ light.py
@@ -2,14 +2,17 @@ import json
 import time
 
 def lightProbe(clue, display):
- print(json.dumps({'name': 'light', 'value': 'na'}))
- display[0].text="light"
+ print(json.dumps({'name': 'light', 'value': 'NA'}))
+ display[0].text="NA"
  display.show()
  time.sleep(0.5)
  while True:
   if clue.button_a or clue.button_b: break
   clue.stop_tone()
-  val = sum(list(clue.color)) / 2.25
+  val = scale(sum(list(clue.color)))
   clue.start_tone(val)
   time.sleep(0.02)
  clue.stop_tone()
+
+def scale(val):
+ return round((((val - 0) * (4000 - 1000)) / (1020 - 0)) + 1000)
