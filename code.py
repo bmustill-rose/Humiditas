20345d171f85167fdedbb837009c3e507fa8a544
bmustill-rose <services+github@benmr.com>
2022-06-11 00:13:23 +0100

    Play sleepSound after 1 minute of inactivity (commit is broken)

--- code.py
+++ code.py
@@ -13,15 +13,21 @@ time.sleep(0.1)
 utils.playSoundSequence(clue, config.startupSound)
 print(json.dumps("Ok"))
 
+runningTime = time.monotonic()
+
 while True:
+ if time.monotonic() > runningTime+60: utils.playSoundSequence(clue, config.sleepSound)
  display.show()
  if clue.button_a and clue.button_b:
   light.lightProbe(clue, display if config.useDisplay else None);
   if config.useDisplay: display[0].text = "Ok"
+  runningTime = time.monotonic()
  elif clue.button_a: 
   utils.surfaceSensorData(clue, display if config.useDisplay else None, {"name": "humidity", "value": round(clue.humidity)}, config.noteDigitMappings)
   if config.useDisplay: display[0].text = "Ok"
+  runningTime = time.monotonic()
  elif clue.button_b:
   utils.surfaceSensorData(clue, display if config.useDisplay else None, {"name": "temperature", "value": round(clue.temperature)}, config.noteDigitMappings)
   if config.useDisplay: display[0].text = "Ok"
+  runningTime = time.monotonic()
  time.sleep(0.05)
